78d644d874bdac1f566215413653a5f91cd818cb
Ray888Ray <ilimirgashev@gmail.com>
2022-11-22 16:44:53 +0600

    gitignore

---
+++ source/source/urls.py
@@ -0,0 +1,28 @@
+"""source URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from webapp.views import IndexView, InfoView, AddView, UpdatedView, DeleteView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('tracker/<int:pk>/', InfoView.as_view(), name='info'),
+    path('tracker/add', AddView.as_view(), name='add'),
+    path('tracker/<int:pk>/update/', UpdatedView.as_view(), name='update'),
+    path('tracker/<int:pk>/delete/', DeleteView.as_view(), name='delete'),
+
+]
