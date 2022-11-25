a7b5f676324815c4c37aea74407ff1ad123d4925
Ray888Ray <ilimirgashev@gmail.com>
2022-11-25 18:14:41 +0600

    FromView was added to update and create

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from webapp.models import Tracker, Status, Type
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from webapp.models import Tracker
 from django.views.generic import TemplateView
 from webapp.forms import TackerForm
+from django.views.generic import FormView
+from .base_validator import FormView as HandMadeView
 
 
 # Create your views here.
@@ -24,50 +26,6 @@ class InfoView(TemplateView):
        return context
 
 
-class AddView(TemplateView):
-    template_name = 'add.html'
-
-    def get(self, request, *args, **kwargs):
-        form = TackerForm()
-        return render(request, "add.html", {'form': form})
-
-    def post(self, request, *args, **kwargs):
-        form = TackerForm(data=request.POST)
-        if form.is_valid():
-            types = form.cleaned_data.pop('type')
-            new_tracker = Tracker.objects.create( **form.cleaned_data)
-            new_tracker.type.set(types)
-            return redirect('info', pk=new_tracker.pk)
-        else:
-            return render(request, 'add.html', {'form': form})
-
-
-class UpdatedView(TemplateView):
-    template_name = 'update.html'
-
-    def get(self, request, *args, **kwargs):
-        tracker = get_object_or_404(Tracker, pk=kwargs['pk'])
-        form = TackerForm(initial={
-            'short_description': tracker.short_description,
-            'content': tracker.content,
-            'type': tracker.type.all(),
-            'status': tracker.status})
-        return render(request, 'update.html', {'form': form})
-
-    def post(self, request, *args, **kwargs):
-        tracker = get_object_or_404(Tracker, pk=kwargs['pk'])
-        form = TackerForm(data=request.POST)
-        if form.is_valid():
-            tracker.short_description = form.cleaned_data.get('short_description')
-            tracker.content = form.cleaned_data.get('content')
-            tracker.status = form.cleaned_data.get('status')
-            tracker.save()
-            tracker.type.set(form.cleaned_data['type'])
-            return redirect('info', pk=tracker.pk)
-        else:
-            return render(request, 'update.html', {'form': form})
-
-
 class DeleteView(TemplateView):
     template_name = 'delete.html'
 
@@ -79,3 +37,50 @@ class DeleteView(TemplateView):
         tracker = get_object_or_404(Tracker, pk=kwargs['pk'])
         tracker.delete()
         return redirect('index')
+
+
+class AddView(HandMadeView):
+   template_name = 'add.html'
+   form_class = TackerForm
+
+   def form_valid(self, form):
+       types = form.cleaned_data.pop('type')
+       self.tracker = Tracker.objects.create(**form.cleaned_data)
+       self.tracker.type.set(types)
+       return super().form_valid(form)
+
+   def get_redirect_url(self):
+       return reverse('info', kwargs={'pk': self.tracker.pk})
+
+
+
+
+
+class UpdatedView(FormView):
+    template_name = 'update.html'
+    form_class = TackerForm
+
+    def dispatch(self, request, *args, **kwargs):
+        self.tracker = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tracker'] = self.tracker
+        return context
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.tracker
+        return kwargs
+
+    def form_valid(self, form):
+        self.tracker = form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('info', kwargs={'pk': self.tracker.pk})
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        return get_object_or_404(Tracker, pk=pk)
