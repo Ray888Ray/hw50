c54f24e722eda685dd7505c998a6626b333855d0
Ray888Ray <ilimirgashev@gmail.com>
2022-12-02 18:45:18 +0600

    Seach class was added

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -12,11 +12,9 @@ class TackerForm(forms.ModelForm):
     def clean_short_description(self):
         short_description = self.cleaned_data['short_description']
         if len(short_description) < 5:
-            self.add_error('short_description', ValidationError('This field should be at least %(length)d symbols long!'
-                                                                , code='too_short', params={'length': 5}))
+            self.add_error('short_description', ValidationError('This field should be at least %(length)d symbols long!', code='too_short', params={'length': 5}))
         if len(short_description) > 20:
-            self.add_error('short_description', ValidationError('This field should be at least %(length)d symbols long!'
-                                                                , code='too_long', params={'length': 20}))
+            self.add_error('short_description', ValidationError('This field should be at least %(length)d symbols long!', code='too_long', params={'length': 20}))
         return short_description
 
     def clean(self):
@@ -24,14 +22,14 @@ class TackerForm(forms.ModelForm):
         if cleaned_data['short_description'] == cleaned_data['content']:
             raise ValidationError("Text of the article should not duplicate it's title!")
         if len(cleaned_data['content']) < 75:
-            self.add_error('short_description', ValidationError('This field should be at least %(length)d symbols long!'
-                                                                , code='too_short', params={'length': 75}))
-        if len(cleaned_data['content']) > 200:
-            self.add_error('short_description', ValidationError('This field should be at least %(length)d symbols long!'
-                                                                , code='too_long', params={'length': 200}))
+            self.add_error('content', ValidationError('This field should be at least %(length)d symbols long!', code='too_short', params={'length': 75}))
+        elif len(cleaned_data['content']) > 2000:
+            self.add_error('content', ValidationError('This field should be no more %(length)d symbols long!', code='too_long', params={'length': 75}))
         return cleaned_data
 
 
+class SimpleSearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label="Search")
 
 
 
