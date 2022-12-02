1169a489c8839e0327481bae3f36ec6cecee1394
Ray888Ray <ilimirgashev@gmail.com>
2022-12-02 18:46:30 +0600

    Custom ListViews was created

--- source/webapp/base_validator.py
+++ source/webapp/base_validator.py
@@ -1,33 +1,46 @@
 from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.views.generic import View, TemplateView
+
+
+class ListView(TemplateView):
+    context_key = 'objects'
+    model = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context[self.context_key] = self.get_objects()
+        return context
+
+    def get_objects(self):
+        return self.model.objects.all()
 
 
 class FormView(View):
-   form_class = None
-   template_name = None
-   redirect_url = ''
+    form_class = None
+    template_name = None
+    redirect_url = ''
 
-   def get(self, request, *args, **kwargs):
-       form = self.form_class()
-       context = self.get_context_data(form=form)
-       return render(request, self.template_name, context=context)
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        context = self.get_context_data(form=form)
+        return render(request, self.template_name, context=context)
 
-   def post(self, request, *args, **kwargs):
-       form = self.form_class(data=request.POST)
-       if form.is_valid():
-           return self.form_valid(form)
-       else:
-           return self.form_invalid(form)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
-   def form_valid(self, form):
-       return redirect(self.get_redirect_url())
+    def form_valid(self, form):
+        return redirect(self.get_redirect_url())
 
-   def form_invalid(self, form):
-       context = self.get_context_data(form=form)
-       return render(self.request, self.template_name, context=context)
+    def form_invalid(self, form):
+        context = self.get_context_data(form=form)
+        return render(self.request, self.template_name, context=context)
 
-   def get_context_data(self, **kwargs):
-       return kwargs
+    def get_context_data(self, **kwargs):
+        return kwargs
 
-   def get_redirect_url(self):
-       return self.redirect_url
+    def get_redirect_url(self):
+        return self.redirect_url
