92cd706c97743d10010e043119b663c3628a38a1
Ray888Ray <ilimirgashev@gmail.com>
2022-12-06 18:04:39 +0600

    new admin panels

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,25 +1,11 @@
 from django.contrib import admin
-from webapp.models import Tracker, Type, Status
-
+from webapp.models import Tracker, Type, Status, Project
 
 # Register your models here.
 
-
-class TrackerAdmin(admin.ModelAdmin):
-    list_display = ['id', 'short_description', 'content', 'type', 'status']
-
-    def type(self, object):
-        for type in object.type.all():
-            return type
-    list_display_links = ['short_description']
-    list_filter = ['content']
-    search_fields = ['short_description', 'content']
-    exclude = []
-    readonly_fields = ['created_at', 'updated_at']
-
-
-admin.site.register(Tracker, TrackerAdmin)
+admin.site.register(Tracker)
 admin.site.register(Type)
 admin.site.register(Status)
+admin.site.register(Project)
 
 
