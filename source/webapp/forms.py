7e8f7397d7d003bdb693b208a626125b3610b0e2
Ray888Ray <ilimirgashev@gmail.com>
2022-12-06 18:05:09 +0600

    new froms for projects

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from django.forms import widgets, ValidationError
-from webapp.models import Tracker
+from webapp.models import Tracker, Project
 
 
 class TackerForm(forms.ModelForm):
     class Meta:
         model = Tracker
-        fields = ['short_description', 'content', 'type', 'status']
+        fields = ['short_description', 'content', 'type', 'status', 'projects_fk']
         widgets = {'type': widgets.CheckboxSelectMultiple}
 
     def clean_short_description(self):
@@ -26,6 +26,14 @@ class TackerForm(forms.ModelForm):
         elif len(cleaned_data['content']) > 2000:
             self.add_error('content', ValidationError('This field should be no more %(length)d symbols long!', code='too_long', params={'length': 75}))
         return cleaned_data
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        # fields = ['title', 'description']
+        fields = ['title', 'description', 'started_at', 'finished_at']
+        widgets = {'started_at': widgets.SelectDateWidget, 'finished_at': widgets.SelectDateWidget}
 
 
 class SimpleSearchForm(forms.Form):
