5a439add11b1b4aa9ae1b2dab932573ec6d64e84
Ray888Ray <ilimirgashev@gmail.com>
2022-12-16 16:03:06 +0600

    urls from accounts and webapp

--- source/source/urls.py
+++ source/source/urls.py
@@ -14,23 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from webapp.views import IndexView, InfoView, AddView, UpdatedView, TrackerDeleteView
-from webapp.views.project_views import ProjectIndexView, ProjectAddView, ProjectView, \
-    ProjectDeleteView, ProjectUpdateView
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', ProjectIndexView.as_view(), name='project_index'),
-    path('project/<int:pk>/', ProjectView.as_view(), name='project_info'),
-    path('project/add/', ProjectAddView.as_view(), name='project_add'),
-    path('project/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
-    path('project/<int:pk>/add/tracker/', AddView.as_view(), name='add'),
-    path('project/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
-
-    path('tracker/list/', IndexView.as_view(), name='index'),
-    path('tracker/<int:pk>/', InfoView.as_view(), name='info'),
-    path('tracker/<int:pk>/update/', UpdatedView.as_view(), name='update'),
-    path('tracker/<int:pk>/delete/', TrackerDeleteView.as_view(), name='delete'),
-
+    path('', include('webapp.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
