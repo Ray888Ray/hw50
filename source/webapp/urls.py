2bae25d52d9d7ae5c8e86d86807dbcc2cc5949f7
Ray888Ray <ilimirgashev@gmail.com>
2022-12-16 16:01:38 +0600

    urls for webapp only

---
+++ source/webapp/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from webapp.views import IndexView, InfoView, AddView, UpdatedView, TrackerDeleteView
+from webapp.views.project_views import ProjectIndexView, ProjectAddView, ProjectView, \
+    ProjectDeleteView, ProjectUpdateView
+
+app_name = 'webapp'
+
+urlpatterns = [
+    path('', ProjectIndexView.as_view(), name='project_index'),
+    path('project/<int:pk>/', ProjectView.as_view(), name='project_info'),
+    path('project/add/', ProjectAddView.as_view(), name='project_add'),
+    path('project/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('project/<int:pk>/add/tracker/', AddView.as_view(), name='add'),
+    path('project/<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
+
+    path('tracker/list/', IndexView.as_view(), name='index'),
+    path('tracker/<int:pk>/', InfoView.as_view(), name='info'),
+    path('tracker/<int:pk>/update/', UpdatedView.as_view(), name='update'),
+    path('tracker/<int:pk>/delete/', TrackerDeleteView.as_view(), name='delete'),
+
+]
