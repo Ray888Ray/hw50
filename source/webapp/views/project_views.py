ba2f7bf16faf21ebc0f9c6f20aaa3d34d08f2c3d
Ray888Ray <ilimirgashev@gmail.com>
2022-12-22 18:22:06 +0600

    search form added

--- source/webapp/views/project_views.py
+++ source/webapp/views/project_views.py
@@ -2,8 +2,10 @@ from django.shortcuts import reverse, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from webapp.models import Project
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-from webapp.forms import ProjectForm
+from webapp.forms import ProjectForm, SimpleSearchForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from django.utils.http import urlencode
 
 
 class ProjectIndexView(ListView):
@@ -15,6 +17,36 @@ class ProjectIndexView(ListView):
     def get_queryset(self):
         soft_deletion = Project.objects.filter(is_deleted=False)
         return soft_deletion
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        if self.form.is_valid():
+            self.search_value = self.form.cleaned_data['search']
+        return super().get(request, *args, **kwargs)
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.search_value:
+            queryset = Project.objects.filter(is_deleted=False)
+        if self.search_value:
+            query = Q(title__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+            context['search'] = self.search_value
+        return context
 
 
 class ProjectView(DetailView):
