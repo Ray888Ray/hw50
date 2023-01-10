b93b5ba6bccf1e33e0bbfc2aebbc4e22c1b1c3ae
Ray888Ray <ilimirgashev@gmail.com>
2023-01-10 19:26:52 +0600

    new usrls

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from accounts.views import login_view, logout_view, RegisterView
+from accounts.views import login_view, logout_view, RegisterView, UserDetailView, UsersList
+from django.conf import settings
+from django.conf.urls.static import static
 app_name = 'accounts'
 
 
@@ -7,5 +9,7 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    # path('register/', register_view, name='register'),
-]
+    path('profile/<int:pk>/', UserDetailView.as_view(), name='profile'),
+    path('users/', UsersList.as_view(), name='user_list' ),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
